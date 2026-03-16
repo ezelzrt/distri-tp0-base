5f6a8e9fd8db3368ea47c66dc5290a4bb52fac1d
ezelzrt <ezlazarte@fi.uba.ar>
2026-03-16 17:37:56 -0300

    add timeout to nc command

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,7 +6,7 @@ SERVER_PORT=12345
 TEST_MSG="es igual"
 
 docker run --rm --network "$NETWORK" nicolaka/netshoot \
-  sh -c "printf '%s' '$TEST_MSG' | nc $SERVER_HOST $SERVER_PORT | grep -q '^$TEST_MSG$'"
+  sh -c "printf '%s' '$TEST_MSG' | nc -w 3 -q 1 $SERVER_HOST $SERVER_PORT | grep -q '^$TEST_MSG$'"
 
 if [ $? -eq 0 ]; then
   echo "action: test_echo_server | result: success"
